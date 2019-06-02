5ea2510b0b6353d6700565e0ddcd5402d9bbec91
Mottel Zirkind <shachna@gmail.com>
2019-06-02 13:14:15 -0400

    Added method to fetch a movie.

--- src/app/models/moviedb.service.ts
+++ src/app/models/moviedb.service.ts
@@ -8,16 +8,23 @@ import {map} from 'rxjs/operators';
 })
 export class MoviedbService {
   baseUrl = 'https://api.themoviedb.org/3/';
-  apikey = 'api_key=9406ed0141baddd3f8169037803f4f2f';
+  apikey = '?api_key=9406ed0141baddd3f8169037803f4f2f';
 
   constructor(public http: HttpClient) { }
 
+  // Fix this so it uses a custom datatype
   movieSearch(query: string): Observable<any> {
-    const url = this.baseUrl + 'search/movie?' + this.apikey + '&query=' + query;
+    const url = this.baseUrl + 'search/movie' + this.apikey + '&query=' + query;
     return this.http.get(url).pipe(
       map(res => {
         return (res as any).results;
       })
     );
+  }
+
+  // Fix this so it uses a custom datatype
+  fetchMovieById(mid: string): Observable<any> {
+    const url = this.baseUrl + 'movie/' + mid + this.apikey;
+    return this.http.get(url);
   }
 }
